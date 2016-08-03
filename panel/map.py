4186f41d6dd86c306666104cd49ceea58ae8ef56
Serverfire <info@serverfire.net>
2016-08-03 15:06:53 +0430

    clean up send text

--- panel/map.py
+++ panel/map.py
@@ -2,32 +2,33 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 
 
-from matrix.python.samples.samplebase import SampleBase
 from matrix.python.rgbmatrix import graphics
+from matrix.python.rgbmatrix import RGBMatrix
+
 
 import time
 
-class RunText(SampleBase):
-    def __init__(self, *args, **kwargs):
-        super(RunText, self).__init__(*args, **kwargs)
 
-    def Run(self):
-        offscreenCanvas = self.matrix.CreateFrameCanvas()
-        font = graphics.Font()
-        font.LoadFont("../../fonts/7x13.bdf")
-        textColor = graphics.Color(255, 255, 0)
-        pos = offscreenCanvas.width
-        myText = "Hello World!"
+def Run():
+    matrix = RGBMatrix(6, 1, 1)
+    matrix.pwmBits = 11
+    matrix.brightness = 100
+    offscreenCanvas = matrix.CreateFrameCanvas()
+    font = graphics.Font()
+    font.LoadFont("../../fonts/7x13.bdf")
+    textColor = graphics.Color(255, 255, 0)
+    pos = offscreenCanvas.width
+    myText = "Hello World!"
 
-        while True:
-            offscreenCanvas.Clear()
-            len = graphics.DrawText(offscreenCanvas, font, pos, 10, textColor, myText)
-            pos -= 1
-            if (pos + len < 0):
-                pos = offscreenCanvas.width
+    while True:
+        offscreenCanvas.Clear()
+        len = graphics.DrawText(offscreenCanvas, font, pos, 10, textColor, myText)
+        pos -= 1
+        if (pos + len < 0):
+            pos = offscreenCanvas.width
 
-            time.sleep(0.05)
-            offscreenCanvas = self.matrix.SwapOnVSync(offscreenCanvas)
+        time.sleep(0.05)
+        offscreenCanvas = matrix.SwapOnVSync(offscreenCanvas)
 
 
 
@@ -35,9 +36,7 @@ class RunText(SampleBase):
 def show_page(request) :
     if not request.user.is_authenticated():
         return redirect('/login')
-    parser = RunText()
-    if (not parser.process()):
-        parser.print_help()
+    Run()
     return render(request, 'map.html')
 
 
