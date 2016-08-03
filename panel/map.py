af49931e5d997f35774319cd9a49ea94aa468033
Serverfire <info@serverfire.net>
2016-08-03 12:50:52 +0430

    test send text

--- panel/map.py
+++ panel/map.py
@@ -2,8 +2,45 @@ from django.shortcuts import render
 from django.contrib.auth import logout
 from django.shortcuts import redirect
 from django.contrib.auth import authenticate, login
+from mainserver.matrix.python.samples.samplebase import SampleBase
+from mainserver.matrix.python.samples.rgbmatrix import graphics
+
+import time
+
+class RunText(SampleBase):
+    def __init__(self, *args, **kwargs):
+        super(RunText, self).__init__(*args, **kwargs)
+
+    def Run(self):
+        offscreenCanvas = self.matrix.CreateFrameCanvas()
+        font = graphics.Font()
+        font.LoadFont("../../fonts/7x13.bdf")
+        textColor = graphics.Color(255, 255, 0)
+        pos = offscreenCanvas.width
+        myText = "Hello World!"
+
+        while True:
+            offscreenCanvas.Clear()
+            len = graphics.DrawText(offscreenCanvas, font, pos, 10, textColor, myText)
+            pos -= 1
+            if (pos + len < 0):
+                pos = offscreenCanvas.width
+
+            time.sleep(0.05)
+            offscreenCanvas = self.matrix.SwapOnVSync(offscreenCanvas)
+
+
+
 
 def show_page(request) :
     if not request.user.is_authenticated():
         return redirect('/login')
+    parser = RunText()
+    if (not parser.process()):
+        parser.print_help()
     return render(request, 'map.html')
+
+
+
+
+
