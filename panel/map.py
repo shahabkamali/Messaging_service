96663cf370255bc5e67813f60140a608f1fd66b0
shahabkamali <shahabkamalicom@gmail.com>
2016-08-24 12:27:41 +0430

    add map list page

--- panel/map.py
+++ panel/map.py
@@ -5,103 +5,103 @@ import sys
 sys.path.append('/home/pi/mainserver/matrix/python/samples')
 
 
-from samplebase import SampleBase
+# from samplebase import SampleBase
 
 
 import time
 
-class RunText(SampleBase):
-    def __init__(self, *args, **kwargs):
-        super(RunText, self).__init__(*args, **kwargs)
-
-
-
-
-"""
-from .core import RGBMatrix, FrameCanvas
-import graphics
-import time
-
-
-def RunText():
-    matrix = RGBMatrix(32, 6, 1)
-    matrix.pwmBits = 11
-    matrix.brightness = 100
-
-    offscreenCanvas = matrix.CreateFrameCanvas()
-
-
-    font = graphics.Font()
-
-    font.LoadFont("../matrix/fonts/7x13.bdf")
-
-    textColor = graphics.Color(255, 255, 0)
-
-    pos = offscreenCanvas.width
-    myText = "Hello World!"
-
-
-    while True:
-        offscreenCanvas.Clear()
-        len = graphics.DrawText(offscreenCanvas, font, pos, 10, textColor, myText)
-        pos -= 1
-        if (pos + len < 0):
-            pos = offscreenCanvas.width
-        time.sleep(0.05)
-        offscreenCanvas = matrix.SwapOnVSync(offscreenCanvas)
-
+# class RunText(SampleBase):
+#     def __init__(self, *args, **kwargs):
+#         super(RunText, self).__init__(*args, **kwargs)
+#
+#
+#
+#
+# """
+# from .core import RGBMatrix, FrameCanvas
+# import graphics
+# import time
+#
+#
+# def RunText():
+#     matrix = RGBMatrix(32, 6, 1)
+#     matrix.pwmBits = 11
+#     matrix.brightness = 100
+#
+#     offscreenCanvas = matrix.CreateFrameCanvas()
+#
+#
+#     font = graphics.Font()
+#
+#     font.LoadFont("../matrix/fonts/7x13.bdf")
+#
+#     textColor = graphics.Color(255, 255, 0)
+#
+#     pos = offscreenCanvas.width
+#     myText = "Hello World!"
+#
+#
+#     while True:
+#         offscreenCanvas.Clear()
+#         len = graphics.DrawText(offscreenCanvas, font, pos, 10, textColor, myText)
+#         pos -= 1
+#         if (pos + len < 0):
+#             pos = offscreenCanvas.width
+#         time.sleep(0.05)
+#         offscreenCanvas = matrix.SwapOnVSync(offscreenCanvas)
+#
+# import threading
+# import thread
+# def worker():
+#     print "in worker"
+#     parser = SimpleSquare()
+#     if (not parser.process()):
+#         parser.print_help()
+#
+#
+# def SimpleSquare():
+#     matrix = RGBMatrix(32, 6, 1)
+#     matrix.pwmBits = 11
+#     matrix.brightness = 100
+#
+#     offsetCanvas = matrix.CreateFrameCanvas()
+#     while True:
+#         for x in range(0, matrix.width):
+#             offsetCanvas.SetPixel(x, x, 255, 255, 255)
+#             offsetCanvas.SetPixel(offsetCanvas.height - 1 - x, x, 255, 0, 255)
+#
+#         for x in range(0, offsetCanvas.width):
+#             offsetCanvas.SetPixel(x, 0, 255, 0, 0)
+#             offsetCanvas.SetPixel(x, offsetCanvas.height - 1, 255, 255, 0)
+#
+#         for y in range(0, offsetCanvas.height):
+#             offsetCanvas.SetPixel(0, y, 0, 0, 255)
+#             offsetCanvas.SetPixel(offsetCanvas.width - 1, y, 0, 255, 0)
+#         offsetCanvas = matrix.SwapOnVSync(offsetCanvas)
+#
+#
+# def RunGraphics():
+#
+#     matrix = RGBMatrix(32, 6, 1)
+#     matrix.pwmBits = 11
+#     matrix.brightness = 100
+#     canvas = matrix;
+#     font = graphics.Font()
+#     font.LoadFont("4x6.bdf")
+#
+#     red = graphics.Color(255, 0, 0)
+#     graphics.DrawLine(canvas, 5, 5, 22, 13, red)
+#
+#     green = graphics.Color(0, 255, 0)
+#     graphics.DrawCircle(canvas, 15, 15, 10, green)
+#
+#     blue = graphics.Color(0, 0, 255)
+#     graphics.DrawText(canvas, font, 2, 10, blue, "Text")
+#
+#     time.sleep(10)  # show display for 10 seconds before exit
+#
+#     """
 import threading
-import thread
-def worker():
-    print "in worker"
-    parser = SimpleSquare()
-    if (not parser.process()):
-        parser.print_help()
-
-
-def SimpleSquare():
-    matrix = RGBMatrix(32, 6, 1)
-    matrix.pwmBits = 11
-    matrix.brightness = 100
-
-    offsetCanvas = matrix.CreateFrameCanvas()
-    while True:
-        for x in range(0, matrix.width):
-            offsetCanvas.SetPixel(x, x, 255, 255, 255)
-            offsetCanvas.SetPixel(offsetCanvas.height - 1 - x, x, 255, 0, 255)
-
-        for x in range(0, offsetCanvas.width):
-            offsetCanvas.SetPixel(x, 0, 255, 0, 0)
-            offsetCanvas.SetPixel(x, offsetCanvas.height - 1, 255, 255, 0)
-
-        for y in range(0, offsetCanvas.height):
-            offsetCanvas.SetPixel(0, y, 0, 0, 255)
-            offsetCanvas.SetPixel(offsetCanvas.width - 1, y, 0, 255, 0)
-        offsetCanvas = matrix.SwapOnVSync(offsetCanvas)
-
-
-def RunGraphics():
-
-    matrix = RGBMatrix(32, 6, 1)
-    matrix.pwmBits = 11
-    matrix.brightness = 100
-    canvas = matrix;
-    font = graphics.Font()
-    font.LoadFont("4x6.bdf")
-
-    red = graphics.Color(255, 0, 0)
-    graphics.DrawLine(canvas, 5, 5, 22, 13, red)
-
-    green = graphics.Color(0, 255, 0)
-    graphics.DrawCircle(canvas, 15, 15, 10, green)
-
-    blue = graphics.Color(0, 0, 255)
-    graphics.DrawText(canvas, font, 2, 10, blue, "Text")
-
-    time.sleep(10)  # show display for 10 seconds before exit
-
-    """
-
 from django.http import HttpResponse
 def show_page(request) :
     #parser = RunText()
@@ -116,18 +116,13 @@ def show_page(request) :
     else:
         import os,logging
         text = request.POST['text']
-        os.system("(while :; do echo '%s' ; sleep 0.2 ; done) | sudo /home/pi/mainserver/matrix/text-example -f /home/pi/mainserver/matrix/fonts/8x13B.bdf -y8 -c2 -C0,0,255 -c12 -r16h -b10 " % text)
-        i = 0
-        while True:
-            i += 1
-            time.sleep(1)
-            if i > 10 :
-                break
-
+        def run_bash():
+            os.system("(while :; do echo '%s' ; sleep 0.2 ; done) | sudo /home/pi/mainserver/matrix/text-example -f /home/pi/mainserver/matrix/fonts/8x13B.bdf -y8 -c2 -C0,0,255 -c12 -r16h -b10 " % text)
+        t = threading.Thread(target=run_bash)
+        t.start()
+        time.sleep(10)
         os.system("sudo ps aux | grep 'text-example'  | awk '{print $2}' | xargs kill -9" )
-
         return HttpResponse("done")
-
 
 
 
