a625800d5a7d4e63208bd6d5d152791e3258d28b
serverfire <info@serverfire.net>
2016-09-21 12:51:52 +0330

    fix map image

--- map/models.py
+++ map/models.py
@@ -1,8 +1,13 @@
 from __future__ import unicode_literals
-
+from django.conf import settings
 from django.db import models
 from jsonfield import JSONField
 import os
+from django.core.files.storage import FileSystemStorage
+from django.db.models.signals import pre_delete
+from django.dispatch.dispatcher import receiver
+
+
 
 
 class Building(models.Model):
@@ -23,13 +28,34 @@ class Floor(models.Model):
 def content_file_name(instance, filename):
     ext = filename.split('.')[-1]
     print 'inctance', type(instance)
-    filename = "%s.%s" % (instance.pk, ext)
+    filename = "%s.%s" % (instance.mapname, ext)
     return os.path.join('map/picture', filename)
 
 
 class Map(models.Model):
-    mapname = models.CharField(max_length=100)
+    mapname = models.CharField(max_length=100,unique=True)
     floor = models.ForeignKey(Floor)
-    picture = models.ImageField(upload_to=content_file_name)
+    picture = models.FileField(upload_to=content_file_name)
     markers = JSONField(blank=True, null=True)
 
+
+@receiver(pre_delete, sender=Map)
+def map_delete(sender, instance, **kwargs):
+     if instance.picture:
+        if os.path.isfile(instance.picture.path):
+            os.remove(instance.picture.path)
+
+
+@receiver(models.signals.pre_save, sender=Map)
+def auto_delete_file_on_change(sender, instance, **kwargs):
+    if not instance.pk:
+        return False
+    try:
+        old_file = Map.objects.get(pk=instance.pk).picture
+    except Map.DoesNotExist:
+        return False
+
+    new_file = instance.picture
+    if not old_file == new_file:
+        if os.path.isfile(old_file.path):
+            os.remove(old_file.path)
--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import url
 import views
-from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
